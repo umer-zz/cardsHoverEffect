f8837325ea26bf3ac3cbbc4adf542f93ba5ec358
unknown <zzumii67@gmail.com>
2023-01-01 15:21:11 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const handleonMouseMove = e => {
+    const { currentTarget: target } = e
+    const rect = target.getBoundingClientRect();
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
+    target.style.setProperty('--mouse-x', `${x}px`)
+    target.style.setProperty('--mouse-y', `${y}px`)
+}
+
+for (const card of document.querySelectorAll('.card')) {
+    card.onmousemove = e => handleonMouseMove(e)
+}
